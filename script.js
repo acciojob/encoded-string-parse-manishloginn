748a6de6c8cd373bf264c32f08ffbae5041b15b4
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-02-16 20:02:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const parseCode = (str) => {
   // your code here
+ 
+  const [firstName, lastName, id] = str.split("0").filter(Boolean);
+  return { firstName, lastName, id };
 };
-
+ 
 // Do not change the code below
 const str = prompt("Enter str: ");
 alert(JSON.stringify(parseCode(str)));
